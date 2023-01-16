31e20a7574dc244145a8e4026d7d69b45cd69460
Avinash Kalwale <avinashkalwale12@gmail.com>
2023-01-16 17:01:55 +0530

    para.color change

---
+++ script.js
@@ -0,0 +1,9 @@
+const hTitle=document.getElementById("h1")
+const pTitle=document.getElementById("p")
+const box=document.getElementById("box")
+hTitle.style.textAlign="center";
+pTitle.style.backgroundColor="skyblue";
+box.style.width="700px";
+box.style.marginLeft="20%";
+box.style.padding="5px";
+console.log("Hello");
